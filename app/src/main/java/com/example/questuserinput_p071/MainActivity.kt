36a7a5d5310ac4a34c9df0833d113bf348737bcb
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-01 10:42:45 +0700

    Memanggil composable RegistrationForm di MainActivity

--- app/src/main/java/com/example/questuserinput_p071/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput_p071/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput_P071Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    RegistrationForm (
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestUserInput_P071Theme {
-        Greeting("Android")
-    }
-}

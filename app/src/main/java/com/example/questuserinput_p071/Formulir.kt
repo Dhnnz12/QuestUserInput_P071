0f2f531506e72614893020600c71641a265bbafb
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-01 10:57:14 +0700

    Mengubah warna border textfield saat tidak aktif menjadi hitam

--- app/src/main/java/com/example/questuserinput_p071/Formulir.kt
+++ app/src/main/java/com/example/questuserinput_p071/Formulir.kt
@@ -83,7 +83,7 @@ fun RegistrationForm() {
                     shape = RoundedCornerShape(8.dp),
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = darkPurple,
-                        unfocusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.Black,
                     ),
                     keyboardOptions = KeyboardOptions.Default.copy(
                         imeAction = ImeAction.Next
@@ -165,7 +165,7 @@ fun RegistrationForm() {
                     shape = RoundedCornerShape(8.dp),
                     colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = darkPurple,
-                        unfocusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.Black,
                     ),
                     keyboardOptions = KeyboardOptions.Default.copy(
                         imeAction = ImeAction.Done

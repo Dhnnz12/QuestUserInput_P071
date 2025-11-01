5783257f8eaba5c99a961332dc2bbc920519b799
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-01 10:29:27 +0700

    Membuat tampilan header dengan Column dan Box

--- app/src/main/java/com/example/questuserinput_p071/Formulir.kt
+++ app/src/main/java/com/example/questuserinput_p071/Formulir.kt
@@ -29,4 +29,20 @@ fun RegistrationForm() {
     val lightPurple = Color(0xFFE0D6FF)
     val darkPurple = Color(0xFF6750A4)
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(lightPurple)
+                .padding(24.dp),
+            contentAlignment = Alignment.Center
+        )
+        {
+
+        }
+
 }
